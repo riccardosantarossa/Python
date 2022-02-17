74f98135dd88ff39bc77addbe1b2e83d6339f332
riccardosantarossa <rikysanta03@gmail.com>
2022-02-17 15:29:09 +0100

    Tentativi randomici

--- ImmagineDaMatrici.py
+++ ImmagineDaMatrici.py
@@ -3,6 +3,8 @@
 import numpy as np
 from PIL import Image
 
-arr = np.random.randint(0, high=255, size=(500,650))
+#arr = np.random.randint(0, high=255, size=(500,650))
+
+#arr[:,5] = np.zeros((650))
 img = Image.fromarray(arr)
 img.show()
