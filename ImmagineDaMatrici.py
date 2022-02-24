c1acee66e64edcfa8c4ed233f7e5ad2cc9935ca8
riccardosantarossa <rikysanta03@gmail.com>
2022-02-24 08:59:45 +0100

    Immagine con pixel a caso

--- ImmagineDaMatrici.py
+++ ImmagineDaMatrici.py
@@ -3,8 +3,27 @@
 import numpy as np
 from PIL import Image
 
-#arr = np.random.randint(0, high=255, size=(500,650))
+nrighe = 650
+arr = np.random.randint(0, high=255, size=(650,650))
 
-#arr[:,5] = np.zeros((650))
+arr[:,5] = np.zeros((650))
+arr[:,6] = np.zeros((650))
+arr[:,7] = np.zeros((650))
+arr[:,8] = np.zeros((650))
+arr[6,:] = np.zeros((650))
+arr[7,:] = np.zeros((650))
+arr[8,:] = np.zeros((650))
+arr[20,:] = np.zeros((650))
+arr[21,:] = np.zeros((650))
+arr[22,:] = np.zeros((650))
+arr[23,:] = np.zeros((650))
+arr[40,:] = np.zeros((650))
+arr[41,:] = np.zeros((650))
+arr[42,:] = np.zeros((650))
+arr[43,:] = np.zeros((650))
+arr[44,:] = np.zeros((650))
+arr[45,:] = np.zeros((650))
+
+
 img = Image.fromarray(arr)
 img.show()
