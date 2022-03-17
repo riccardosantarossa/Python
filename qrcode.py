fe579fab206030ece83f0f83420a17d399a31cef
Riccardo Santarossa <brudo1968@libero.it>
2022-03-17 13:42:07 +0100

    Roba di Toner

--- qrcode.py
+++ qrcode.py
@@ -1,13 +1,14 @@
+#Lettore QR code
 import cv2
 import keyboard
-import datetime
- 
-cap = cv2.VideoCapture(0)
-codec = cv2.VideoWriter_fourcc(*'MJPG')#codifica video
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+codec = cv2.VideoWriter_fourcc(*'MJPG')  # codifica video
 out = None
-rec=False
+rec = False
 ret, frame = cap.read()
 decoder = cv2.QRCodeDetector()
+flag = 0
 
 if(not ret):
     print("webcam non rilevata")
@@ -17,21 +18,29 @@ while(cap.isOpened()):
     _, frame = cap.read()
     if(rec):
         out.write(frame)
-
-    cv2.imshow("webcam", frame)
+    #cv2.imshow("webcam", frame)
     k = cv2.waitKey(1)
     data, points, _ = decoder.detectAndDecode(frame)
     if points is not None:
-        print('Decoded data: ' + data) 
         points = points[0]
         for i in range(len(points)):
             pt1 = [int(val) for val in points[i]]
             pt2 = [int(val) for val in points[(i + 1) % 4]]
             cv2.line(frame, pt1, pt2, color=(255, 0, 0), thickness=3)
 
-cap.release()#libera webcam
-cv2.destroyAllWindows()
+        if data != '':
+            print('Decoded data: ' + data)
+            cv2.putText(frame, data, (100, 100),
+                        cv2.FONT_HERSHEY_DUPLEX, 1, (125, 246, 55), 3)
+            cv2.imwrite('qr.jpg', frame)
+            break
 
+    cv2.imshow("webcam", frame)
+
+cap.release()  # libera webcam
+cv2.imshow("qr.jpg", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 
