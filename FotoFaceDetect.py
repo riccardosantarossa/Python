d8cd1cefc52fac1e28b7f5e72b649fe045bcd101
Riccardo Santarossa <brudo1968@libero.it>
2022-03-17 14:12:54 +0100

    Funziona decentemente e c'è Linus

--- FotoFaceDetect.py
+++ FotoFaceDetect.py
@@ -3,12 +3,14 @@
 import cv2
 from cv2 import COLOR_BGR2GRAY
 
-img= cv2.imread()
-gray = cv2.cvtColor(img,cv2,COLOR_BGR2GRAY)
+img= cv2.imread("memelinus.jpg")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-faces = face_cascade.detectMultiScale(gray,1.05,2)
+faces = face_cascade.detectMultiScale(gray,1.1,2)
+
+print("Volti trovati = %d" % len(faces) )
 
 for(x,y,w,h) in faces: 
     cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
