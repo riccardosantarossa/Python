811b3f92760f207b7d97978fe28bc4f8df0f90d6
Riccardo Santarossa <brudo1968@libero.it>
2022-03-19 10:09:24 +0100

    Completi e commentati

--- FotoFaceDetect.py
+++ FotoFaceDetect.py
@@ -3,18 +3,25 @@
 import cv2
 from cv2 import COLOR_BGR2GRAY
 
+#Leggo l'immagine selezionata
 img= cv2.imread("memelinus.jpg")
+#Converto in bianco e nero
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
+#Importo il dataset per il riconoscimento facciale
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
+#Parametri per il riconoscimento
 faces = face_cascade.detectMultiScale(gray,1.1,2)
 
+#Stampo quanti volti ci sono nella foto in console
 print("Volti trovati = %d" % len(faces) )
 
+#Disegno i rettangoli intorno alle facce
 for(x,y,w,h) in faces: 
     cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
 
+#Mostro l'immagine e rilascio le risorse
 cv2.imshow("image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows
--- VideofaceDetect.py
+++ VideofaceDetect.py
@@ -1,5 +1,6 @@
 #SANTAROSSA RICCARDO
 
+#Importazione delle librerie
 from fileinput import filename
 import cv2
 import logging as log
@@ -8,17 +9,23 @@ from time import sleep
 import keyboard
 from datetime import datetime
 
+#Seleziono il file da cui importare il dataset del riconoscimento
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
+#Salvo sul file di log gli eventuali errori
 log.basicConfig(filename='webcam.log',level=log.INFO)
 
+#Sorgente video di default (0)
 video_capture = cv2.VideoCapture(0)
 anterior = 0
 
+#CODEC
 codec = cv2.VideoWriter_fourcc(*'MJPG')
 out = None
 
 while 1==1:
+    
+    #Ricerca la telecamera o dà errore
     if not video_capture.isOpened():
         print('Telecamera non trovata.')
         sleep(5)
@@ -27,37 +34,48 @@ while 1==1:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
+    #Converte ogni frame in bianco e nero per la detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    #Algoritmo che riconosce la faccia
     faces = faceCascade.detectMultiScale(
         gray,
+        #Scompone l'immagine in sezioni del 2%
         scaleFactor=1.2,
         minNeighbors=5,
         minSize=(30, 30)
     )
         
-    # Draw a rectangle around the faces
+    # Disegna i rettangoli intorno ai volti
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, ), 2)
+        #Ascolta gli input da tastiera
         if keyboard.is_pressed('b'):
             _, frame = video_capture.read()
+            #Converte l'immagine in scala di grigi
             gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
             now = datetime.now()
+            #Creo il file che conterrà l'immagine
             filename = now.strftime("%Y%m%d%H%M%S") + ".jpg"
+            #Salvo il risultato dello screenshot in bianco e nero dentro il file
             cv2.imwrite(filename,cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 0, ), 2))
         elif keyboard.is_pressed('v'):
             if out == None: 
+                #Registra il video e lo salva in formato AVI con CODEC MJPEG
                 out=cv2.VideoWriter('output.avi', codec, 10., (640,480))
         elif keyboard.is_pressed('f'):
             now = datetime.now()
+            #Creo il file che conterrà l'immagine
             filename = now.strftime("%Y%m%d%H%M%S") + ".jpg"
+            #Scatto una foto a colori e la salvo
             snap = cv2.imwrite(filename, frame)
         elif keyboard.is_pressed('t'):
             font = cv2.FONT_HERSHEY_SIMPLEX
             _, frame = video_capture.read()
+            #La funzione putText viene chiamata con tutti i suoi parametri (immagine, testo, posizione, font, dimensioni e colore)
             cv2.putText(frame, str(datetime.now()), (2,50), font, 1, (255,0,0), 2, cv2.LINE_AA)
 
-
+    #Stampa il numero di volti in tempo reale in console
     if anterior != len(faces):
         anterior = len(faces)
         print("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
@@ -65,7 +83,7 @@ while 1==1:
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
-
+    #Se si preme Q si chiude il programma
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
